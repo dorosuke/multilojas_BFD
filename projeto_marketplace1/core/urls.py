4ef23b486db385ded1d463554dce1e1f5ab13ea0
dorosuke <projetodorosuki@gmail.com>
2026-05-26 07:29:54 -0300

    fix: remove rota busca-global quebrada

--- projeto_marketplace1/core/urls.py
+++ projeto_marketplace1/core/urls.py
@@ -42,7 +42,6 @@ from .views import (
     seller_products_page,
     seller_store_page,
     store_page,
-    busca_unificada_view,
 )
 
 urlpatterns = [
@@ -87,6 +86,4 @@ urlpatterns = [
     path('front/cadastro/', register_page, name='front-register'),
     path('front/recuperar-senha/', forgot_password_page, name='front-forgot-password'),
     path('front/perfil/', profile_page, name='front-profile'),
-    path('busca-global/', busca_unificada_view, name='busca_unificada'),
 ]
-    
